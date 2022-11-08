49ef8c7fc7c1e53e66c95d8c9aa3331346e5344b
PascualVera <pascualveradev@gmail.com>
2022-11-08 14:12:04 +0100

    questions rendering correctly

--- src/App.js
+++ src/App.js
@@ -1,114 +1,123 @@
-import './App.css';
-import React, { useState }  from 'react';
-import {
-  Route,
-  Routes,
-  BrowserRouter,
-  Link,
-  
-} from "react-router-dom";
-import Questions from './Components/Questions';
-import Output from './Components/Output';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { Route, Routes, BrowserRouter, Link } from "react-router-dom";
+import Questions from "./Components/Questions";
+import Output from "./Components/Output";
 
 function App() {
-  // React States
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
+	// React States
+	const getQuestions = async () => {
+		const data = await fetch("http://localhost:5243/question/all");
+		const qData = await data.json();
+		setQuestions(() => {
+			console.log(qData);
+			return qData;
+		});
+	};
+	const postRegister = async user => {};
+	const [questions, setQuestions] = useState();
+	const [errorMessages, setErrorMessages] = useState({});
+	const [isSubmitted, setIsSubmitted] = useState(false);
+	useEffect(() => {
+		getQuestions();
+	}, []);
+	// User Login info
+	const database = [
+		{
+			username: "user1",
+		},
+		{
+			username: "user2",
+		},
+	];
 
-  // User Login info
-  const database = [
-    {
-      username: "user1",
-    },
-    {
-      username: "user2",
-    }
-  ];
+	const errors = {
+		uname: "invalid username",
+	};
 
-  const errors = {
-    uname: "invalid username",
-  };
+	const handleSubmit = event => {
+		//Prevent page reload
+		event.preventDefault();
 
-  const handleSubmit = (event) => {
-    //Prevent page reload
-    event.preventDefault();
+		var { uname } = document.forms[0];
 
-    var { uname } = document.forms[0];
+		// Find user login info
+		const userData = database.find(user => user.username === uname.value);
 
-    // Find user login info
-    const userData = database.find((user) => user.username === uname.value);
+		// Compare user info
+		if (userData) {
+			if (userData.username !== uname.value) {
+				// Invalid
+				setErrorMessages({ name: "uname", message: errors.uname });
+			} else {
+				setIsSubmitted(true);
+			}
+		}
+	};
 
-    // Compare user info
-    if (userData) {
-      if (userData.username !== uname.value) {
-        // Invalid 
-        setErrorMessages({ name: "uname", message: errors.uname });
-      } else {
-        setIsSubmitted(true);
-      }
-    } 
-  };
+	// Generate JSX code for error message
+	const renderErrorMessage = name =>
+		name === errorMessages.name && (
+			<div className="error">{errorMessages.message}</div>
+		);
 
-  // Generate JSX code for error message
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
-    );
+	// JSX code for login form
+	const renderForm = (
+		<div className="form">
+			<form onSubmit={handleSubmit}>
+				<div className="input-container">
+					<label>Login with Email </label>
+					<input type="text" name="uname" required />
+					{renderErrorMessage("uname")}
+				</div>
+				<div className="button-container">
+					<input type="submit" />
+				</div>
+			</form>
+			<form onSubmit={handleSubmit}>
+				<div className="input-container">
+					<label>Create User</label>
+					<input type="text" name="uname" required />
+					{renderErrorMessage("uname")}
+				</div>
+				<div className="button-container">
+					<input type="submit" />
+				</div>
+			</form>
+		</div>
+	);
 
-  // JSX code for login form
-  const renderForm = (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label>Login with Email </label>
-          <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
-        </div>
-        <div className="button-container">
-          <input type="submit" />
-        </div>
-      </form>
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label>Create User</label>
-          <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
-        </div>
-        <div className="button-container">
-          <input type="submit" />
-        </div>
-      </form>
-    </div>
-    
-  );
+	return (
+		<React.Fragment>
+			<div className="app">
+				<div className="login-form">
+					<div className="title">Sign In</div>
+					{isSubmitted ? (
+						<div>
+							<p>User is successfully logged in</p>
+							<br></br>
+							<button>
+								<Link to="/question">Continue</Link>
+							</button>
+						</div>
+					) : (
+						renderForm
+					)}
+				</div>
+			</div>
 
-  return (
-    <React.Fragment>
-    <div className="app">
-      <div className="login-form">
-        <div className="title">Sign In</div>
-        {isSubmitted ? <div>
-          <p>User is successfully logged in</p>
-          <br></br>
-          <button>
-            <Link to="/question">Continue</Link>
-          </button>
-          </div> : renderForm}
-      </div>
-  </div>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path="questions"
+						element={<Questions questions={questions}></Questions>}
+					></Route>
 
-    <BrowserRouter>
-          <Routes>
-            <Route path="questions" 
-            element={<Questions></Questions>}></Route>
-            
-            <Route path="output" 
-            element={<Output></Output>}></Route>
-            </Routes>
-        </BrowserRouter>
-    </React.Fragment>
-    
-  );
+					<Route path="output" element={<Output></Output>}></Route>
+				</Routes>
+			</BrowserRouter>
+		</React.Fragment>
+	);
 }
 
 export default App;
--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -2,39 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 
-function Questions() {
-  return (
-    <form className="question">
-      <div>
-        <div className="login-form">
-          <select typeof="asking">
-            <option>--Question 1--</option>
-          </select>
-        </div>
-      </div>
-      <br></br>
-      <div>
-        <div className="login-form">
-          <select typeof="asking">
-            <option>--Question 2--</option>
-          </select>
-        </div>
-      </div>
-      <br></br>
-      <div>
-        <div className="login-form">
-          <select typeof="asking">
-            <option>--Question 3--</option>
-          </select>
-          </div>
-          <div className="button-container">
-            <button >
-              <Link to="/output">Check</Link>
-            </button>
-        </div>
-      </div>
-    </form>
-  );
-};
+function Questions({ questions }) {
+	return (
+		<form className="question">
+			{questions &&
+				questions.map(question => {
+					return (
+						<div>
+							<div className="login-form">
+								<h2>{question.questiontext}</h2>
+								<select typeof="asking">
+									{question.answer.map(answer => {
+										return <option>{answer.option}</option>;
+									})}
+								</select>
+							</div>
+						</div>
+					);
+				})}
+
+			<div className="button-container">
+				<button>
+					<Link to="/output">Check</Link>
+				</button>
+			</div>
+		</form>
+	);
+}
 
 export default Questions;
